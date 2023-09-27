9b82a3e3180c8d394f8c7a51daa0a002540c6ffc
Amresh-Kumar1 <amresh@wizni.com>
2023-09-27 11:16:08 +0530

    added loop docs

--- docs/src/app/docs/pages/loops/loops.component.ts
+++ docs/src/app/docs/pages/loops/loops.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./loops.component.scss']
 })
 export class LoopsComponent {
+  forLoopExample=`
+  for (let i = 0; i < 3; i++)
+    printLn("hello", i); // braces not needed for single statement
+  
+    // prints hello 0
+  //        hello 1
+  //        hello 2
+  
+  let Items = ["Item 1", "Item 2", "Item 3"];
+  for(let i=0;i<3;i++){
+    let item = Items[i];
+    print(item);
+  }
+  
+  // prints   Item 1
+  //          Item 2
+  //          Item 3`;
 
+  whileLoopExample=`
+let count = 0;
+while (count < 5) {
+  // Code to repeat
+  count++;
+}
+print(count); //5`;
 }
