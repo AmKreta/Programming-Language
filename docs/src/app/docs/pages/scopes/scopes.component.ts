7a8f724f334c2513e59920bde3c0e990199b449b
Amresh-Kumar1 <amresh@wizni.com>
2023-09-27 11:47:29 +0530

    added scope docs

--- docs/src/app/docs/pages/scopes/scopes.component.ts
+++ docs/src/app/docs/pages/scopes/scopes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ScopesTypes } from './definationList';
 
 @Component({
   selector: 'app-scopes',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./scopes.component.scss']
 })
 export class ScopesComponent {
+  scopesTypes = ScopesTypes;
 
+  scopeExamples = [
+    {
+      codeSnippet: `
+      let globalVar = "I'm global";
+
+      function outerFunction() {
+          let outerVar = "I'm in outerFunction";
+          
+          function innerFunction() {
+              let innerVar = "I'm in innerFunction";
+              let globalVar = "I'm a local var Shadowing global var";
+              console.log(innerVar); // Accesses the inner variable
+              console.log(outerVar); // Accesses the outer variable
+              console.log(globalVar); // Accesses the locally defined globalVar
+          }
+      
+          innerFunction();
+          
+          console.log(innerVar); // Error: innerVar is not defined here
+          console.log(outerVar); // Accesses the outer variable
+          console.log(globalVar); // Accesses the global variable
+      }
+      
+      outerFunction();
+      `,
+      description: "In this example, you can see how variables are scoped within functions and how shadowing works. The innerVar is accessible only within innerFunction, and when it's not found there, the outer scopes are checked. The outerVar is accessible within outerFunction, and the globalVar is accessible globally."
+    }
+  ];
 }
