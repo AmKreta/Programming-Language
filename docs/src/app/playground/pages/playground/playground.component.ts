99f85fda4386105f8cdc535b925b0833987b0e4c
Amresh-Kumar1 <amresh@wizni.com>
2023-09-27 23:10:22 +0530

    created playground

--- docs/src/app/playground/pages/playground/playground.component.ts
+++ docs/src/app/playground/pages/playground/playground.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CodeModel } from '@ngstack/code-editor';
+import { Subject, scan } from 'rxjs';
+
+// wasm Module
 
 @Component({
   selector: 'app-playground',
   templateUrl: './playground.component.html',
   styleUrls: ['./playground.component.scss']
 })
-export class PlaygroundComponent {
+export class PlaygroundComponent implements OnInit, OnDestroy {
   theme = 'vs-dark';
 
   codeModel: CodeModel = {
@@ -17,9 +20,9 @@ export class PlaygroundComponent {
 
   options = {
     contextmenu: true,
-    lineNo:true,
-    bracketPairColorization:true,
-    formatOnPaste:true,
+    lineNo: true,
+    bracketPairColorization: true,
+    formatOnPaste: true,
     "editor.codeActionsOnChange": {
       "source.fixAll": false,
       "source.fixAll.eslint": false, // If you're using ESLint
@@ -28,9 +31,51 @@ export class PlaygroundComponent {
       enabled: true
     }
   };
-  
 
-  onCodeChanged(value:string) {
-    console.log('CODE', value);
+  wasmModule: any;
+  code = '';
+  consoleLog: any; // original console log function
+  codeOutput$ = new Subject<string>();
+  codeOutput: string[] = [];
+
+  public onCodeChanged(value: string) {
+    this.code = value;
+  }
+
+  public runCode() {
+    if (this.wasmModule)
+      (this.wasmModule as any).run(this.code);
+  }
+
+  public clear() {
+    this.codeOutput = [];
+  }
+
+  ngOnInit() {
+    const script = document.createElement('script');
+    script.src = 'assets/wasm-compiled-program/main.js';
+    document.getElementsByTagName('head')[0].appendChild(script);
+    script.onload = () => {
+      console.log(this.wasmModule);
+      this.wasmModule = (window as any).Module;
+    }
+
+    // intercepting console.log
+    this.consoleLog = console.log;
+    console.log = ((originalConsoleLog: Function) => {
+      return (...args: any[]) => {
+        let textContent = Array.prototype.slice.call(args).join(' ');
+        originalConsoleLog(textContent);
+        this.codeOutput$.next(textContent);
+      }
+    })(console.log.bind(console))
+
+    this.codeOutput$
+      //.pipe(scan((acc, val)=>acc + val || ''))
+      .subscribe(res => this.codeOutput.push(res));
+  }
+
+  ngOnDestroy(): void {
+    console.log = this.consoleLog;
   }
 }
