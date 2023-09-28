156bfe2256cbbf0a937418f5ed721bdc85a4bc71
Amresh-Kumar1 <amresh@wizni.com>
2023-09-28 10:11:06 +0530

    replaced post-increment in loop examples docs

--- docs/src/app/docs/pages/loops/loops.component.ts
+++ docs/src/app/docs/pages/loops/loops.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class LoopsComponent {
   forLoopExample=`
-  for (let i = 0; i < 3; i++)
+  for (let i = 0; i < 3; i=i+1)
     printLn("hello", i); // braces not needed for single statement
   
     // prints hello 0
@@ -15,7 +15,7 @@ export class LoopsComponent {
   //        hello 2
   
   let Items = ["Item 1", "Item 2", "Item 3"];
-  for(let i=0;i<3;i++){
+  for(let i=0;i<3;i=i+1){
     let item = Items[i];
     print(item);
   }
@@ -28,7 +28,7 @@ export class LoopsComponent {
 let count = 0;
 while (count < 5) {
   // Code to repeat
-  count++;
+  count=count+1;
 }
 print(count); //5`;
 }
