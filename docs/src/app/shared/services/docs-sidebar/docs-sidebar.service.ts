3a90daa9c0bbff6858afc841eb2b173a7f4b8d82
Amresh-Kumar1 <amresh@wizni.com>
2023-10-03 23:24:34 +0530

    fixed side-bar display on screen size change

--- docs/src/app/shared/services/docs-sidebar/docs-sidebar.service.ts
+++ docs/src/app/shared/services/docs-sidebar/docs-sidebar.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
+import { IsSmallScreenService } from '../isSmallScreen/is-small-screen.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +11,21 @@ export class DocsSidebarService {
   showHambuggerMenu$ = new BehaviorSubject<boolean>(false);
   showDocsMenu = false;
 
-  constructor(public router: Router) {
+  constructor(
+    private router: Router,
+    private isSmallScreenService:IsSmallScreenService
+  ) {
     this.router.events.subscribe(ev => {
       if (ev instanceof NavigationEnd)
         this.showHambuggerMenu$.next(ev.url.includes("/docs"));
     });
+    this.isSmallScreenService.isSmallScreen$.subscribe(res=>{
+      if(!res)
+        this.showDocsMenu = true;
+    })
   }
 
   toggleDocsMenu() {
-    this.showDocsMenu = !this.showDocsMenu;
+      this.showDocsMenu = !this.showDocsMenu;
   }
 }
